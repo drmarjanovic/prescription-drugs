a69367a2b07a75c2f32e6a0b592e999c20f61df1
Dragutin Marjanović <dmarjanovic94@gmail.com>
2021-09-01 01:27:04 +0200

    Handle non-existing SFTP file

--- src/main/scala/com/github/markaya/app/http/Routes.scala
+++ src/main/scala/com/github/markaya/app/http/Routes.scala
@@ -8,23 +8,33 @@ import zhttp.service.Server
 import zio.RIO
 import zio.blocking.Blocking
 import zio.json._
-
-import java.io.IOException
+import zio._
+import zio.stream.ZStream
 
 final case class Routes(config: AppConfig) {
 
   private val sftp = new Sftp(config.sftp)
 
-  private val routes: HttpApp[Blocking, IOException] = HttpApp.collect {
+  private val routes = HttpApp.collectM {
     case Method.GET -> Root / "health"                       =>
-      Response.jsonString(AppInfo.up.toJsonPretty)
+      UIO.succeed(Response.jsonString(AppInfo.up.toJsonPretty))
     case Method.GET -> Root / "api" / "prescriptions" / name =>
-      val content = HttpData.fromStream(sftp.read(name))
-      Responses.file(name, content)
+      sftp
+        .read(name)
+        .runCollect
+        .map(chunk => Responses.file(name, HttpData.fromStream(ZStream.fromChunk(chunk))))
+        .orElseSucceed {
+          Responses.notFound(
+            title = "File not found",
+            detail = s"The requested resource '$name' does not exist."
+          )
+        }
     case _ -> path                                           =>
-      Responses.notFound(
-        title = "Page not found",
-        detail = s"The requested URL $path was not found on this server. That's all we know."
+      UIO.succeed(
+        Responses.notFound(
+          title = "Page not found",
+          detail = s"The requested URL $path was not found on this server. That's all we know."
+        )
       )
   }
 
